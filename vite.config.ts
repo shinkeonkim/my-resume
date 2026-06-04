eca1caf46130c87bda5fd0cf7168fc797f7dbd37
shinkeonkim <dev.shinkeonkim@gmail.com>
2026-06-04 15:44:17 +0900

    feat: github page 배포로 shinkeonkim.com/my-resume URL 통해 들어올 수 있도록 처리

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES_BASE ?? '/',
   plugins: [
     vue(),
     vueJsx(),
